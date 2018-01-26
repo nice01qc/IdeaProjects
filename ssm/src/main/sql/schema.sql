8230e86f45eabe7ff7bfca66c7afe6ea89bb4746
nice01qc <734522310@qq.com>
2018-01-26 22:13:00 +0800

    upgrade

--- ssm/src/main/sql/schema.sql
+++ ssm/src/main/sql/schema.sql
@@ -4,15 +4,15 @@ CREATE TABLE `book` (
   `name` varchar(100) NOT NULL COMMENT '图书名称',
   `number` int(11) NOT NULL COMMENT '馆藏数量',
   PRIMARY KEY (`book_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='图书表'
+) ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='图书表';
 
 -- 初始化图书数据
-INSERT INTO `book` (`book_id`, `name`, `number`)
+INSERT INTO book` (`book_id`, `name`, `number`);
 VALUES
 	(1000, 'Java程序设计', 10),
 	(1001, '数据结构', 10),
 	(1002, '设计模式', 10),
-	(1003, '编译原理', 10)
+	(1003, '编译原理', 10);
 
 -- 创建预约图书表
 CREATE TABLE `appointment` (
