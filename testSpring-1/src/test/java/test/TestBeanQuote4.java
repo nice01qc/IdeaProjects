2ae77fd5ad65e915192bac01cd8fb4f4c5ad3a60
nice01qc <734522310@qq.com>
2018-01-28 17:31:46 +0800

    upgrade

--- testSpring-1/src/test/java/test/TestBeanQuote4.java
+++ testSpring-1/src/test/java/test/TestBeanQuote4.java
@@ -7,12 +7,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * 测试引用其他bean
  */
 public class TestBeanQuote4 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchFieldException {
         ClassPathXmlApplicationContext pFactory = new ClassPathXmlApplicationContext(new String[]{"parentbean.xml"});
         ApplicationContext factory = new ClassPathXmlApplicationContext(new String[]{"beanscopy4.xml"},pFactory);
         Boss boss = (Boss) factory.getBean("boss");
         Secretary secretary =  factory.getBean("secretary",Secretary.class);
         System.out.println(boss);
         System.out.println(secretary);
+        Class a = TestBeanQuote4.class;
+
     }
 }
